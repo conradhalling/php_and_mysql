b05347565c429e19c0086604303aeb5eb6711bc1
conradhalling <conrad.halling@icloud.com>
2025-01-09 17:28:37 -0500

    Section A Chapter 1
    Finish six script exercises.
    
    Finish navigation page.

---
+++ section_a/chapter01/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Section A Chapter 1</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+
+  <body>
+    <header class="center">
+      <h1>PHP & MySQL</h1>
+      <h2>Section A: Basic Programming Instructions</h2>
+      <h3>Chapter 1 Variables, Expressions, & Operators</h3>
+    </header>
+    <main class="center">
+      <h4>Code Exercises</h4>
+      <ul>
+        <li><a href="variables.php">Variables</a></li>
+        <li><a href="updating-variables.php">Updating Variables</a></li>
+        <li><a href="associative-arrays.php">Associative Arrays</a></li>
+        <li><a href="indexed-arrays.php">Indexed Arrays</a></li>
+        <li><a href="updating-arrays.php">Updating Arrays</a></li>
+        <li><a href="multidimensional-arrays.php">Multidimensional Arrays</a>
+        </li>
+        <li><a href="echo-shorthand.php">Echo Shorthand</a></li>
+        <li><a href="arithmetic-operators.php">Arithmetic Operators</a></li>
+        <li><a href="string-operator.php.php">String Operator</a></li>
+        <li><a href="comparison-operators.php">Comparison Operators</a></li>
+        <li><a href="logical-operators.php">Logical Operators</a></li>
+        <li><a href="example.php">Example</a></li>
+      </ul>
+    </main>
+  </body>
+
+</html>
