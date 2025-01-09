fe79722f44f27e99e6f372fa9f5fe87c2f7a9fc6
conradhalling <conrad.halling@icloud.com>
2025-01-09 14:39:05 -0500

    Section A Introduction
    Finish Section A Introduction scripts.
    
    Add a .gitignore file to ignore .DS_store files.

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP &amp; MySQL (My Code)</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+
+  <body>
+    <header class="center">
+      <h1 class="center">PHP &amp; MySQL (My Code)</h1>
+    </header>
+    <main class="center">
+      <nav>
+        <h2>Section A: Basic Programming Instructions</h2>
+        <ul>
+          <li><a href="section_a/chapter00/">Introduction</a></li>
+          <li><a href="section_a/chapter01/">Chapter 1: Variables,
+              Expressions, and
+              Operators</a></li>
+          <li><a href="section_a/chapter02/">Chapter 2: Control
+              Structures</a>
+          </li>
+          <li><a href="section_a/chapter03/">Chapter 3: Functions</a></li>
+          <li><a href="section_a/chapter04/">Chapter 4: Objects</a></li>
+        </ul>
+
+        <h2>Section B: Dynamic Web Pages</h2>
+        <ul>
+          <li><a href="section_b/chapter00/">Introduction</a></li>
+          <li><a href="section_b/chapter05/">Chapter 5: Built-in Functions</a>
+          </li>
+          <li><a href="section_b/chapter06/">Chapter 6: Getting Data from
+              Browsers</a>
+          </li>
+          <li><a href="section_b/chapter07/">Chapter 7: Images and Files</a>
+          </li>
+          <li><a href="section_b/chapter08/">Chapter 8: Dates and Times</a></li>
+          <li><a href="section_b/chapter09/">Chapter 9: Cookies and Sessions</a>
+          </li>
+          <li><a href="section_b/chapter10/">Chapter 10: Error Handling</a></li>
+        </ul>
+
+        <h2>Section C: Database Driven Websites</h2>
+        <ul>
+          <li><a href="section_c/chapter11/">Chapter 11: SQL</a></li>
+          <li><a href="section_c/chapter12/">Chapter 12: Getting Data from a
+              Database</a></li>
+          <li><a href="section_c/chapter13/">Chapter 13: Updatng Data in the
+              Database</a>
+          </li>
+        </ul>
+
+        <h2>Section D: Extending the Application</h2>
+        <ul>
+          <li><a href="section_d/chapter14/">Chapter 14: Refactoring and
+              Dependency Injection</a></li>
+          <li><a href="section_d/chapter15/">Chapter 15: Namespaces and
+              Libraries</a></li>
+          <li><a href="section_d/chapter16/">Chapter 16: Membership</a></li>
+          <li><a href="section_d/chapter17/">Chapter 17: Ading New Features</a>
+          </li>
+        </ul>
+      </nav>
+    </main>
+    <footer class="center">
+      <p class="footer">
+        Updated 2025-01-09
+      </p>
+    </footer>
+  </body>
+
+</html>
---
+++ section_a/chapter00/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Section A Introduction</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+
+  <body>
+    <header class="center">
+      <h1>PHP & MySQL</h1>
+      <h2>Section A: Basic Programming Instructions</h2>
+      <h3>Introduction</h3>
+    </header>
+    <main class="center">
+      <h4>Code Exercises</h4>
+      <ul>
+        <li><a href="test.php">test.php</a></li>
+        <li><a href="echo.php">echo.php</a></li>
+        <li><a href="comment.php">comment.php</a></li>
+      </ul>
+    </main>
+  </body>
+
+</html>
