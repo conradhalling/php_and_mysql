2f7ab3b07df025b96848174a02186c0868771b4e
conradhalling <conrad.halling@icloud.com>
2025-01-11 17:30:40 -0500

    Section A Chapter 4
    Finish the first three coding exercises.

---
+++ section_a/chapter04/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Section A Chapter 4</title>
+    <link rel="stylesheet" href="../../..//style.css">
+  </head>
+
+  <body>
+    <header class="center">
+      <h1><cite>PHP &amp; MySQL</cite></h1>
+      <h2>Section A: Basic Programming Instructions</h2>
+      <h3>Chapter 4 Objects &amp; Classes</h3>
+    </header>
+    <main class="center">
+      <h4>Code Exercises</h4>
+      <ul>
+        <li><a href="objects-and-properties.php">Objects &amp; Properties</a>
+        <li><a href="objects-and-methods.php">Objects &amp; Methods</a>
+        <li><a href="constructor-methods.php">Constructor Methods</a>
+        </li>
+      </ul>
+    </main>
+  </body>
+
+</html>
---
+++ section_a/chapter04/objects-and-properties.php
@@ -0,0 +1,30 @@
+<?php
+// This script accesses the object's properties directly.
+// This is fine for a simple program where the code will not be shared.
+
+class Customer
+{
+    public string $forename;
+    public string $surname;
+    public string $email;
+    public string $password;
+}
+
+class Account
+{
+    public int $number;
+    public string $type;
+    public float $balance;
+}
+
+$customer = new Customer();
+$account = new Account();
+$customer->email = 'ivy@eg.link';
+$account->balance = 1000.00;
+?>
+
+<?php include 'includes/header.php'; ?>
+<p>Email: <?= $customer->email ?></p>
+<p>Balance: $<?= $account->balance ?></p>
+<?php include 'includes/footer.php'; ?>
+
